89601f0de82f1f8bc7b4ff59ee739efe1195b9f3
George Wilson <georgejameswilson@googlemail.com>
2014-04-16 17:30:28 +0100

    Bring us back to the present :P

--- administrator/components/com_localise/helpers/localise.php
+++ administrator/components/com_localise/helpers/localise.php
@@ -2,7 +2,7 @@
 /**
  * @package     Localise
  *
- * @copyright   Copyright (C) 20014 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
