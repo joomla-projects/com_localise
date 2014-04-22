a1bd5c2e7162baceed03163fb7967eef02b7d427
Michael Babker <mbabker@flbab.com>
2014-04-21 19:50:04 -0500

    Correct ZIP filename

--- build/build.sh
+++ build/build.sh
@@ -6,4 +6,4 @@ cp -r ../media/com_localise packaging/media
 mv packaging/admin/localise.xml packaging/localise.xml
 mv packaging/admin/script.php packaging/script.php
 cd packaging
-zip -r ../packages/com_patchtester.zip admin/ media/ localise.xml script.php
+zip -r ../packages/com_localise.zip admin/ media/ localise.xml script.php
