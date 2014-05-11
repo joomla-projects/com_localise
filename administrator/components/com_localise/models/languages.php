1828755637c8548e358ae4f45c2d1354b6ac4ed7
Jean-Marie Simonet <infografjms@gmail.com>
2014-05-11 09:39:13 +0200

    Filtering broken in managers

--- administrator/components/com_localise/models/languages.php
+++ administrator/components/com_localise/models/languages.php
@@ -30,7 +30,7 @@ class LocaliseModelLanguages extends JModelList
 	protected function populateState($ordering = null, $direction = null)
 	{
 		$app  = JFactory::getApplication();
-		$data = $app->input->get('filters');
+		$data = $app->input->get('filters', array(), 'array');
 
 		if (empty($data))
 		{
