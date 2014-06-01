8aabb45ab7b3202b40a992aa53eef5dfd5eef3d8
Jean-Marie Simonet <infografjms@gmail.com>
2014-06-01 07:43:22 +0200

    code style

--- administrator/components/com_localise/views/translations/tmpl/default_head.php
+++ administrator/components/com_localise/views/translations/tmpl/default_head.php
@@ -17,7 +17,7 @@ $listDirn = $this->escape($this->state->get('list.direction'));
 	<th width="100" class="center hidden-phone"><?php echo JText::_('COM_LOCALISE_HEADING_TRANSLATIONS_INFORMATION'); ?></th>
 	<th width="50" class="center"><?php echo JHtml::_('grid.sort', 'COM_LOCALISE_TOOLBAR_PACKAGES_LANGUAGE', 'tag', $listDirn, $listOrder); ?></th>
 	<th width="100" class="center hidden-phone"><?php echo JText::_('COM_LOCALISE_HEADING_LANGUAGES_CLIENT'); ?></th>
-        <th width="250" class="title"><?php echo JHtml::_('grid.sort', 'COM_LOCALISE_HEADING_LANGUAGES_FILES', 'filename', $listDirn, $listOrder); ?></th>
+	<th width="250" class="title"><?php echo JHtml::_('grid.sort', 'COM_LOCALISE_HEADING_LANGUAGES_FILES', 'filename', $listDirn, $listOrder); ?></th>
 	<th width="120" class="center"><?php echo JHtml::_('grid.sort', 'COM_LOCALISE_HEADING_TRANSLATIONS_TRANSLATED', 'completed', $listDirn, $listOrder); ?></th>
 	<th width="120" class="center"><?php echo JHtml::_('grid.sort', 'COM_LOCALISE_HEADING_TRANSLATIONS_PHRASES', 'translated', $listDirn, $listOrder); ?></th>
 	<th width="100" class="hidden-phone"><?php echo JText::_('COM_LOCALISE_HEADING_TRANSLATIONS_AUTHOR'); ?></th>
