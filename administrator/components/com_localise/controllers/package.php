0bba9d1e2221df0fd9be012916c3c938fbbc41ca
Jean-Marie Simonet <infografjms@gmail.com>
2014-06-17 08:46:48 +0200

    Removing useless method

--- administrator/components/com_localise/controllers/package.php
+++ administrator/components/com_localise/controllers/package.php
@@ -108,20 +108,6 @@ class LocaliseControllerPackage extends JControllerForm
 	}
 
 	/**
-	 * Method to get a model object, loading it if required.
-	 *
-	 * @param   string  $name    The model name. Optional.
-	 * @param   string  $prefix  The class prefix. Optional.
-	 * @param   array   $config  Configuration array for model. Optional.
-	 *
-	 * @return  object  The model.
-	 */
-	public function getModel($name = 'Package', $prefix = 'LocaliseModel', $config = array('ignore_request' => false))
-	{
-		return parent::getModel($name, $prefix, $config);
-	}
-
-	/**
 	 * Todo: description missing
 	 *
 	 * @return void
