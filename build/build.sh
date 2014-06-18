32ea9ea2167709158a2dd692c6714f6deff9eec0
javier gomez <javier.gomez.corio@gmail.com>
2014-06-18 13:59:29 +0200

    Update Shell packaging script

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf packaging && mkdir packaging
 rm -rf packages && mkdir packages
-cp -r ../administrator/components/com_localise packaging/admin
+cp -r ../component/admin packaging/admin
 cp -r ../media/com_localise packaging/media
 mv packaging/admin/localise.xml packaging/localise.xml
 mv packaging/admin/script.php packaging/script.php
