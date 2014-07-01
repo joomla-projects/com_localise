5e2dbb61272845950fbe5f2b8ebab8965fa4b9df
Jean-Marie Simonet <infografjms@gmail.com>
2014-07-01 11:19:03 +0200

    Allowing Import of package xml

--- component/admin/views/packages/tmpl/default.php
+++ component/admin/views/packages/tmpl/default.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.modal');
 JHTML::_('stylesheet', 'com_localise/localise.css', null, true);
 JHtml::_('formbehavior.chosen', 'select');
+JHtml::_('behavior.modal');
 
 $listOrder  = $this->escape($this->state->get('list.ordering'));
 $listDirn   = $this->escape($this->state->get('list.direction'));
@@ -86,3 +87,25 @@ $sortFields = $this->getSortFields();
 	</div>
 	<!-- End Content -->
 </form>
+
+<div  id="fileModal" class="modal hide fade">
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h3><?php echo JText::_('COM_LOCALISE_IMPORT_NEW_PACKAGE_HEADER');?></h3>
+	</div>
+	<div class="modal-body">
+		<div class="column">
+			<form method="post" action="<?php echo JRoute::_('index.php?option=com_localise&task=package.uploadFile&file=' . $this->file); ?>"
+				class="well" enctype="multipart/form-data" >
+				<fieldset>
+					<input type="hidden" class="address" name="address" />
+					<input type="file" name="files" required />
+					<input type="submit" value="<?php echo JText::_('COM_LOCALISE_BUTTON_IMPORT');?>" class="btn btn-primary" />
+				</fieldset>
+			</form>
+		</div>
+	</div>
+	<div class="modal-footer">
+		<a href="#" class="btn" data-dismiss="modal"><?php echo JText::_('COM_LOCALISE_MODAL_CLOSE'); ?></a>
+	</div>
+</div>
