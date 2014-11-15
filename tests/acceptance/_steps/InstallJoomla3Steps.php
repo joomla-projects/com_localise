0e44f541afefa08e4b609a112c66d1bbbdb18919
Puneet Kala <puneet.0191@gmail.com>
2014-11-15 14:29:39 +0530

    Updating the Main Installation Step

--- tests/acceptance/_steps/InstallJoomla3Steps.php
+++ tests/acceptance/_steps/InstallJoomla3Steps.php
@@ -1,70 +1,24 @@
 <?php
 /**
  * @package     Joomla
- * @subpackage  Page Class
+ * @subpackage  Step Class
  * @copyright   Copyright (C) 2012 - 2014 All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+
 namespace AcceptanceTester;
+
 /**
  * Class InstallJoomla3Steps
  *
  * @package  AcceptanceTester
  *
- * @link     http://codeception.com/docs/07-AdvancedUsage#StepObjects
- *
  * @since    1.4
+ *
+ * @link     http://codeception.com/docs/07-AdvancedUsage#StepObjects
  */
 class InstallJoomla3Steps extends \AcceptanceTester
 {
-	/**
-	 * @var AcceptanceTester;
-	 */
-	protected $acceptanceTester;
-
-	/**
-	 * Function to InstallJoomla3
-	 *
-	 * @return void
-	 */
-	public function installJoomla3()
-	{
-		$I = $this;
-		$this->acceptanceTester = $I;
-		$I->amOnPage(\InstallJoomla3ManagerPage::$URL);
-		$cfg = $I->getConfig();
-		$this->setField('Site Name', $cfg['site_name']);
-		$this->setField('Your Email', $cfg['admin_email']);
-		$this->setField('Admin Username', $cfg['username']);
-		$this->setField('Admin Password', $cfg['password']);
-		$this->setField('Confirm Admin Password', $cfg['password']);
-		$I->click("//li[@id='database']/a");
-		$I->waitForElement("//li[@id='database'][@class='step active']", 30);
-
-		$this->setDatabaseType($cfg['db_type']);
-		$this->setField('Host Name', $cfg['db_host']);
-		$this->setField('Username', $cfg['db_user']);
-		$this->setField('Password', $cfg['db_pass']);
-		$this->setField('Database Name', $cfg['db_name']);
-		$this->setField('Table Prefix', $cfg['db_prefix']);
-
-		$I->click("//label[@for='jform_db_old1']");
-
-		$I->click("//li[@id='summary']/a");
-
-		if (strtolower($cfg['sample_data']) == "yes")
-		{
-			$this->setSampleData($cfg['sample_data_file']);
-		}
-		else
-		{
-			$this->setSampleData('None');
-		}
-
-		$I->click("//a[@title='Install']");
-		$I->waitForElement("//input[contains(@onclick, 'Install.removeFolder')]", 60);
-	}
-
 	/**
 	 * Function to Populate Values
 	 *
@@ -73,9 +27,9 @@ class InstallJoomla3Steps extends \AcceptanceTester
 	 *
 	 * @return void
 	 */
-	private function setField($label, $value)
+	public function setField($label, $value)
 	{
-		$I = $this->acceptanceTester;
+		$I = $this;
 
 		switch ($label)
 		{
@@ -121,11 +75,10 @@ class InstallJoomla3Steps extends \AcceptanceTester
 	 *
 	 * @return void
 	 */
-	private function setDatabaseType($value)
+	public function setDatabaseType($value)
 	{
-		$I = $this->acceptanceTester;
-		$I->click("//div[@id='jform_db_type_chzn']/a/div/b");
-		$I->click("//div[@id='jform_db_type_chzn']//ul[@class='chzn-results']/li[contains(translate(.,'" . strtoupper($value) . "', '" . strtolower($value) . "'), '" . strtolower($value) . "')]");
+		$I = $this;
+		$I->selectOption(\InstallJoomla3ManagerPage::$dbType, strtolower($value));
 	}
 
 	/**
@@ -135,10 +88,9 @@ class InstallJoomla3Steps extends \AcceptanceTester
 	 *
 	 * @return void
 	 */
-	private function setSampleData($option = 'Default')
+	public function setSampleData($option = 'Default')
 	{
-		$I = $this->acceptanceTester;
-		$I->waitForElement("//label[contains(., '" . $option . "')]", 60);
-		$I->click("//label[contains(., '" . $option . "')]");
+		$I = $this;
+		$I->selectOption(\InstallJoomla3ManagerPage::$sampleFile, $option);
 	}
 }
