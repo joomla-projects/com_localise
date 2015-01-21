2a44dd06062f135ba3ebe47779eaf83c365ffdc4
valc <rafaelgomezcunill@gmail.com>
2015-01-21 22:44:12 +0100

    Special keys cases

--- component/admin/models/fields/key.php
+++ component/admin/models/fields/key.php
@@ -85,6 +85,45 @@ class JFormFieldKey extends JFormField
 			$button2  = '<span style="width:5%;">'
 						. JHtml::_('image', 'com_localise/icon-16-bing-gray.png', '', array('class' => 'pointer'), true) . '</span>';
 		}
+		elseif ($status == 'blocked')
+		{
+		$is_read_only = ' readonly="readonly" ';
+			$onclick = "javascript:document.id(
+						'" . $this->id . "'
+						)
+						.set(
+						'value','" . addslashes(htmlspecialchars($this->element['description'], ENT_COMPAT, 'UTF-8')) . "'
+						);
+						if (document.id('" . $this->id . "').get('value')=='') {document.id('" . $this->id . "').set('class','width-45 blocked');}
+						else {document.id('" . $this->id . "').set('class','width-45 " . $status . "');}";
+			$button  = '<i class="icon-reset hasTooltip return pointer" title="' . JText::_('COM_LOCALISE_TOOLTIP_TRANSLATION_INSERT')
+						. '" onclick="' . $onclick . '"></i>';
+			/* $onclick2 = "javascript:if (typeof(google) !== 'undefined') {
+			var translation='" . addslashes(htmlspecialchars($this->element['description'], ENT_COMPAT, 'UTF-8')) . "';
+				translation=translation.replace('%s','___s');translation=translation.replace('%d','___d');
+				translation=translation.replace(/%([0-9]+)\\\$s/,'___\$1');google.language.translate(translation,
+				Localise.language_src, Localise.language_dest, function(result) {if (result.translation) {
+			  translation = result.translation;
+			  translation = translation.replace('___s','%s');
+			  translation = translation.replace('___d','%d');
+			  translation = translation.replace(/___([0-9]+)/,'%$1\$s');
+			  document.id('" . $this->id . "').set('value',translation);
+			  if (document.id('" . $this->id . "').get('value')=='" . addslashes(htmlspecialchars($this->element['description'], ENT_COMPAT, 'UTF-8'))
+				. "') document.id('" . $this->id . "').set('class','width-45 blocked');
+				else document.id('" . $this->id . "').set('class','width-45 blocked');}
+				else alert(Joomla.JText._('COM_LOCALISE_LABEL_TRANSLATION_GOOGLE_ERROR'));});}
+				else alert(Joomla.JText._('COM_LOCALISE_LABEL_TRANSLATION_GOOGLE_ERROR'));";
+			  $button2 = '<span style="width:5%;">' . JHtml::_('image', 'com_localise/icon-16-google.png', '',
+				array('title' => JText::_('COM_LOCALISE_TOOLTIP_TRANSLATION_GOOGLE'), 'class' => 'hasTooltip pointer',
+				'onclick' => $onclick2), true) . '</span>';
+			  */
+			$token    = JSession::getFormToken();
+			$onclick2 = "javascript:AzureTranslator(this, [], 0, '$token');";
+			$button2  = '<input type="hidden" id="' . $this->id . 'text" value=\''
+						. addslashes(htmlspecialchars($this->element['description'], ENT_COMPAT, 'UTF-8')) . '\' />';
+			$button2 .= '<i class="icon-translate-bing hasTooltip translate pointer" title="'
+						. JText::_('COM_LOCALISE_TOOLTIP_TRANSLATION_AZURE') . '" onclick="' . $onclick2 . '" rel="' . $this->id . '"></i>';
+		}
 		else
 		{
 			$onclick = "javascript:document.id(
