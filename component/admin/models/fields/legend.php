a794cb34c0f826405f62980d2e5f85395c0ba408
Valc <rafaelgomezcunill@gmail.com>
2015-01-22 18:41:31 +0100

    Add blocked string

--- component/admin/models/fields/legend.php
+++ component/admin/models/fields/legend.php
@@ -37,7 +37,7 @@ class JFormFieldLegend extends JFormField
 		$return .= '<tr><td><input class="translated" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_TRANSLATED')
 					. '" readonly="readonly"/></td></tr>';
 		$return .= '<tr><td><input class="untranslatable" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNTRANSLATABLE') . '" readonly="readonly"/></td></tr>';
-		$return .= '<tr><td><input class="blocket" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNTRANSLATABLE') . '" readonly="readonly"/></td></tr>';
+		$return .= '<tr><td><input class="blocked" size="30" type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_BLOCKED') . '" readonly="readonly"/></td></tr>';
 		$return .= '<tr><td><input class="unchanged" size="30"  type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNCHANGED')
 					. '" readonly="readonly"/></td></tr>';
 		$return .= '<tr><td><input class="untranslated" size="30"  type="text" value="' . JText::_('COM_LOCALISE_TEXT_TRANSLATION_UNTRANSLATED')
