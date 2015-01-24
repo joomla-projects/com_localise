4ed08b1516e511e20f3401f89d878ce2340bf097
Valc <rafaelgomezcunill@gmail.com>
2015-01-24 15:15:19 +0100

    Allowed groups fix

--- component/admin/views/translations/tmpl/default_body.php
+++ component/admin/views/translations/tmpl/default_body.php
@@ -74,8 +74,8 @@ $lang = JFactory::getLanguage();
 				<?php echo JHtml::_('jgrid.checkedout', $i, $item->editor, $item->checked_out_time, 'translations.', $canCheckin); ?>
 				<input type="checkbox" id="cb<?php echo $i; ?>" class="hidden" name="cid[]" value="<?php echo $item->id; ?>">
 			<?php endif; ?>
-			<?php if ($item->writable && !$item->error && $canEdit && $have_raw_mode) : ?>
-				<a class="hasTooltip" href="<?php echo JRoute::_('index.php?option=com_localise&task=translation.edit&client='.$item->client.'&tag='.$item->tag.'&filename='.$item->filename.'&storage='.$item->storage.'&id='.LocaliseHelper::getFileId(LocaliseHelper::getTranslationPath($item->client,$item->tag, $item->filename, $item->storage)).($item->filename=='override' ? '&layout=raw' :'')); ?>" title="<?php echo JText::_('COM_LOCALISE_TOOLTIP_TRANSLATIONS_' . ($item->state=='unexisting' ? 'NEW' : 'EDIT')); ?>">
+			<?php if ($item->writable && !$item->error && $canEdit) : ?>
+				<a class="hasTooltip" href="<?php echo JRoute::_('index.php?option=com_localise&task=translation.edit&client='.$item->client.'&tag='.$item->tag.'&filename='.$item->filename.'&storage='.$item->storage.'&id='.LocaliseHelper::getFileId(LocaliseHelper::getTranslationPath($item->client,$item->tag, $item->filename, $item->storage)).(($item->filename=='override' && $have_raw_mode) ? '&layout=raw' :'')); ?>" title="<?php echo JText::_('COM_LOCALISE_TOOLTIP_TRANSLATIONS_' . ($item->state=='unexisting' ? 'NEW' : 'EDIT')); ?>">
 				<?php echo $item->name; ?>.ini
 				</a>
 			<?php elseif (!$canEdit) : ?>
