f772ef070c273364ed3ed5187cc97c00ce1bb628
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-03-16 15:23:50 +0000

    index and varchar length

--- component/admin/sql/install/mysql/install.sql
+++ component/admin/sql/install/mysql/install.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS `#__localise` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `asset_id` int(11) NOT NULL,
-  `path` varchar(255) NOT NULL,
+  `path` varchar(400) NOT NULL DEFAULT '',
   `checked_out` int(10) unsigned NOT NULL DEFAULT '0',
   `checked_out_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `idx_path` (`path`)
+  UNIQUE KEY `idx_path` (`path`(100))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE=utf8mb4_unicode_ci;
