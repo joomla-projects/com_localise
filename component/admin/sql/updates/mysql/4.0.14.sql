468d12511581a46834da9deef0073721c6876a7f
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-03-18 11:11:28 +0000

    Remove unique key and resize idx_path index to 191

--- component/admin/sql/updates/mysql/4.0.14.sql
+++ component/admin/sql/updates/mysql/4.0.14.sql
@@ -14,7 +14,7 @@ ALTER TABLE `#__localise` DROP KEY `idx_path`;
 --
 -- Step 2.1: Limit indexes to first 100 so their max allowed lengths would not get exceeded with utf8mb4.
 -- 
-ALTER TABLE `#__localise` ADD UNIQUE KEY `idx_path` (`path`(100));
+ALTER TABLE `#__localise` ADD KEY `idx_path` (`path`(191));
 
 -- 
 -- Step 2.2: Enlarge columns to avoid data loss on later conversion to utf8mb4
