71c1e02e3daf6a1a662bc030f8e877aa2261fedb
andrepereiradasilva <andrepereiradasilva@users.noreply.github.com>
2016-03-20 14:50:28 +0000

    use dates in sql update files like the core does

--- component/admin/sql/updates/mysql-utf8mb4/4.0.15-2016-03-20.sql
+++ component/admin/sql/updates/mysql-utf8mb4/4.0.15-2016-03-20.sql
@@ -1,16 +1,3 @@
-CREATE TABLE IF NOT EXISTS `#__localise_revised_values` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `client` varchar(255) NOT NULL,
-  `reftag` varchar(6) NOT NULL,
-  `tag` varchar(6) NOT NULL,
-  `filename` varchar(255) NOT NULL,
-  `revised` tinyint(1) NOT NULL DEFAULT 0,
-  `key` varchar(255) NOT NULL,
-  `target_text` mediumtext NOT NULL,
-  `source_text` mediumtext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
-
 -- Step 1 of the UTF-8 Multibyte (utf8mb4) conversion for MySQL.
 -- Drop indexes which will be added again in step 2.
 ALTER TABLE `#__localise` DROP KEY `idx_path`;
